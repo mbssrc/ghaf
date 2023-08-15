4135bcabdf7129fbd5fc4ca8b7d8883c89f59a6e
Manuel Bluhm <manuel@ssrc.tii.ae>
2023-08-15 18:19:07 +0400

    OPA server and network client.
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

---
+++ modules/policy/default.nix
@@ -0,0 +1,50 @@
+# Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.ghaf.policy;
+in
+  with lib; {
+
+    options.ghaf.policy = {
+
+      enable = lib.mkOption {
+        description = "Enable OPA policy network.";
+        type = lib.types.bool;
+        default = true;
+      };
+
+    };
+
+    config = lib.mkIf cfg.enable {
+
+      systemd.services.opa-server = {
+        description = "Ghaf OPA server.";
+        wantedBy = [ "default.target" ];
+        after = [ "network.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.opa-server}/bin/opa run -s";
+          PrivateUsers = true;
+          DynamicUser = true;
+        };
+      };
+
+      systemd.services.opa-client-net = {
+        description = "Ghaf OPA net client.";
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.opa-client-net}/bin/opa version";
+        };
+      };
+
+      environment.systemPackages = with pkgs; [ checksec ];
+
+    };
+
+}
--- targets/vm.nix
+++ targets/vm.nix
@@ -32,7 +32,7 @@
 
               # Enable all the default UI applications
               profiles = {
-                applications.enable = true;
+                # applications.enable = true;
                 #TODO clean this up when the microvm is updated to latest
                 release.enable = variant == "release";
                 debug.enable = variant == "debug";
